d9a53369b8aafb85fee6d03cacf93ff49625bd1c
“tawonaDzapasi” <tdzapsy@gmail.comb>
2022-08-16 12:57:22 +0200

    swagger documentation

--- src/main/java/com/econet/lms/LmsApplication.java
+++ src/main/java/com/econet/lms/LmsApplication.java
@@ -2,12 +2,29 @@ package com.econet.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class LmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LmsApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerConfiguration(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/api/*"))
+				.apis(RequestHandlerSelectors.basePackage("com.econet.lms"))
+				.build();
+	}
+
 }
+
